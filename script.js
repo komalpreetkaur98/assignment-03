2512ef58e31af8c1e9794a3b4da6f3d0f11789c4
komalpreetkaur98 <153228050+komalpreetkaur98@users.noreply.github.com>
2023-12-08 22:02:56 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function openSidebar(){
+document.getElementById("sidebar").style.width= "200px";
+document.getElementById("main").style.width= "250px";
+}
+
+function closeSidebar(){
+    document.getElementById("sidebar").style.width= "0px";
+    document.getElementById("main").style.width= "0px";
+}
+
